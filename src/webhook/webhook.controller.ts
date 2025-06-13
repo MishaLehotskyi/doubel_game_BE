f74a7ce0a5d857ffa9476ac0003dccb081a3c2b3
MishaLehotskyi <eglet777@ukr.net>
2025-06-13 18:11:15 +0300

    webhook

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -134,7 +134,7 @@ export class WebhookController {
     }
 
     const parsed = JSON.parse(rawBody);
-    console.log(parsed);
+    console.log(parsed, parsed.event?.data?.block);
 
     const logs = parsed.event?.data?.block?.logs ?? [];
 
