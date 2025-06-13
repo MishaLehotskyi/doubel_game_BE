811f57d5210f65c9d93b1a1d9ff54e206a4ed974
MishaLehotskyi <eglet777@ukr.net>
2025-06-13 16:41:35 +0300

    webhook

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -6,6 +6,7 @@ import {
   Headers,
   HttpStatus,
 } from '@nestjs/common';
+import { formatUnits, parseUnits } from 'ethers';
 import { Request, Response } from 'express';
 import * as crypto from 'crypto';
 import { TicketService } from '../ticket/ticket.service';
@@ -14,6 +15,7 @@ import { GameService } from '../game/game.service';
 const MINI = '0x62939d201C1c4beFbA34A1DFE85f35B64bc1BcfB'.toLowerCase();
 const STANDARD = '0x0A59e974890265660BC9f3c2182e5cAA9c036723'.toLowerCase();
 const MEGA = '0x740B45a8E7C01AAFC6CD823e5a794F172eE9cCD0'.toLowerCase();
+const DBE = '0x86Aa748baC7BDe8Cd1A7bEf7236Ab4279554b6B6'.toLowerCase();
 
 function isValidSignatureForStringBody(
   body: string,
@@ -64,22 +66,22 @@ export class WebhookController {
     for (const act of activities) {
       const from = act.fromAddress?.toLowerCase();
       const to = act.toAddress?.toLowerCase();
-      const value = Number(act.value);
+      const rawValueHex = act.rawContract?.rawValue;
       const tokenAddress = act.rawContract?.address?.toLowerCase();
 
-      if (
-        tokenAddress !==
-        '0x86Aa748baC7BDe8Cd1A7bEf7236Ab4279554b6B6'.toLowerCase()
-      ) {
-        continue;
-      }
+      if (!rawValueHex || tokenAddress !== DBE) continue;
 
-      console.log(to === MINI, act.value, act);
+      const amount: bigint = BigInt(rawValueHex);
+      const amountStr = formatUnits(amount, 18);
+
+      console.log(to === MINI, amountStr);
       let gameType: 'MINI' | 'STANDARD' | 'MEGA' | null = null;
 
-      if (to === MINI && value === 100.0) gameType = 'MINI';
-      else if (to === STANDARD && value === 500.0) gameType = 'STANDARD';
-      else if (to === MEGA && value === 500.0) gameType = 'MEGA';
+      if (to === MINI && amount === parseUnits('100', 18)) gameType = 'MINI';
+      else if (to === STANDARD && amount === parseUnits('500', 18))
+        gameType = 'STANDARD';
+      else if (to === MEGA && amount === parseUnits('500', 18))
+        gameType = 'MEGA';
 
       //if (gameType) {
       console.log(`🎟 Создание тикета: ${gameType} для ${from}`);
