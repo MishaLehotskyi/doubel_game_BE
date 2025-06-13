4da01e834b6cd4e9609e85330e69e7d86fa94858
MishaLehotskyi <eglet777@ukr.net>
2025-06-13 15:59:53 +0300

    webhook

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,7 @@ async function bootstrap() {
     origin: 'http://localhost:3000',
     credentials: true,
   });
+  app.use('/webhook/transfer', bodyParser.raw({ type: 'application/json' }));
 
   await app.listen(process.env.PORT ?? 3000);
 }
--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -7,18 +7,44 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
+import * as crypto from 'crypto';
+
+function isValidSignatureForStringBody(
+  body: string,
+  signature: string,
+  signingKey: string,
+): boolean {
+  const hmac = crypto.createHmac('sha256', signingKey);
+  hmac.update(body, 'utf8');
+  const digest = hmac.digest('hex');
+  return signature === digest;
+}
 
 @Controller('webhook')
 export class WebhookController {
   @Post('transfer')
   handleWebhook(
     @Req() req: Request,
+    @Headers('x-alchemy-signature') signature: string,
     @Res() res: Response,
-    @Headers() headers: Record<string, string>,
   ) {
-    console.log('📬 Заголовки запроса:');
-    console.dir(headers, { depth: null });
+    const rawBody = (req as any).body.toString('utf8');
+    const signingKey = process.env.ALCHEMY_SIGNING_KEY;
 
-    res.status(HttpStatus.OK).send('✅ Принято');
+    const valid = isValidSignatureForStringBody(
+      rawBody,
+      signature,
+      signingKey as string,
+    );
+
+    if (!valid) {
+      console.warn('🚨 Неверная подпись Alchemy Webhook!');
+      return res.status(HttpStatus.UNAUTHORIZED).send('Invalid signature');
+    }
+
+    const parsed = JSON.parse(rawBody);
+    console.log('✅ Валидная подпись. Данные:', parsed);
+
+    return res.sendStatus(200);
   }
 }
